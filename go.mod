0d81c94611989dc3da762b2241d4aa7ee53d8b72
Hopertz <lugano.ngulwa@gmail.com>
2023-03-10 23:42:16 +0300

    Add commands to the bot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ChessSwahili/ChessSWBot
+
+go 1.20
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("Token"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+    log.Printf("Authorized on account %s", bot.Self.UserName)
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        if update.Message == nil { // ignore any non-Message updates
+            continue
+        }
+
+        if !update.Message.IsCommand() { // ignore any non-command Messages
+            continue
+        }
+
+        // Create a new MessageConfig. We don't have text yet,
+        // so we leave it empty.
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+        // Extract the command from the Message.
+        switch update.Message.Command() {
+        case "start":
+            msg.Text = "Please use this bot to get link of games of Chesswahili team members that are actively playing on Lichess. Type /stop to stop receiving notifications`;"
+        case "stop":
+            msg.Text = "Sorry to see you leave You wont be receiving notifications. Type /start to receive "
+        case "team":
+            msg.Text = "There are 10 members in chesswahili"
+        default:
+            msg.Text = "I don't know that command"
+        }
+
+        if _, err := bot.Send(msg); err != nil {
+            log.Panic(err)
+        }
+    }
+}
