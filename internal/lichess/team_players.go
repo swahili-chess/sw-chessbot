552fe9491c082e28f40ca24945e8040326460ff0
Hopertz <lugano.ngulwa@gmail.com>
2024-08-22 05:49:24 +0300

    fix: since lichess update api

--- internal/lichess/team_players.go
+++ internal/lichess/team_players.go
@@ -14,127 +14,7 @@ import (
 
 type TeamPlayer struct {
 	ID       string `json:"id"`
-	Username string `json:"username"`
-	Perfs    struct {
-		Chess960 struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"chess960"`
-		Atomic struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"atomic"`
-		RacingKings struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"racingKings"`
-		UltraBullet struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"ultraBullet"`
-		Blitz struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"blitz"`
-		KingOfTheHill struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"kingOfTheHill"`
-		Bullet struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"bullet"`
-		Correspondence struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"correspondence"`
-		Horde struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"horde"`
-		Puzzle struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"puzzle"`
-		Classical struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"classical"`
-		Rapid struct {
-			Games  int  `json:"games"`
-			Rating int  `json:"rating"`
-			Rd     int  `json:"rd"`
-			Prog   int  `json:"prog"`
-			Prov   bool `json:"prov"`
-		} `json:"rapid"`
-		Storm struct {
-			Runs  int `json:"runs"`
-			Score int `json:"score"`
-		} `json:"storm"`
-		Racer struct {
-			Runs  int `json:"runs"`
-			Score int `json:"score"`
-		} `json:"racer"`
-		Streak struct {
-			Runs  int `json:"runs"`
-			Score int `json:"score"`
-		} `json:"streak"`
-	} `json:"perfs"`
-	CreatedAt    int64 `json:"createdAt"`
-	Disabled     bool  `json:"disabled"`
-	TosViolation bool  `json:"tosViolation"`
-	Profile      struct {
-		Country    string `json:"country"`
-		Location   string `json:"location"`
-		Bio        string `json:"bio"`
-		FirstName  string `json:"firstName"`
-		LastName   string `json:"lastName"`
-		FideRating int    `json:"fideRating"`
-		UscfRating int    `json:"uscfRating"`
-		EcfRating  int    `json:"ecfRating"`
-		Links      string `json:"links"`
-	} `json:"profile"`
-	SeenAt   int64 `json:"seenAt"`
-	Patron   bool  `json:"patron"`
-	Verified bool  `json:"verified"`
-	PlayTime struct {
-		Total int `json:"total"`
-		Tv    int `json:"tv"`
-	} `json:"playTime"`
-	Title string `json:"title"`
+	Username string `json:"name"`
 }
 
 func FetchTeamPlayers() []db.InsertLichessDataParams {
@@ -179,22 +59,17 @@ func FetchTeamPlayers() []db.InsertLichessDataParams {
 			break
 		}
 
-		pd := db.InsertLichessDataParams{
-
+		dt = append(dt, db.InsertLichessDataParams{
 			LichessID: ctp.ID,
 			Username:  ctp.Username,
-		}
-
-		dt = append(dt, pd)
+		})
 
 	}
 
-	pd := db.InsertLichessDataParams{
+	dt = append(dt, db.InsertLichessDataParams{
 		LichessID: "herald18",
 		Username:  "herald18",
-	}
-
-	dt = append(dt, pd)
+	})
 
 	return dt
 
